0cfdb5f63a866bdce2f453a91714aa1bab05a86a
SergeiShumkov <sergeishumkov72@yahoo.com>
2022-03-21 08:09:14 +0500

    first commit

---
+++ tests/test_example.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+
+def test_first():
+    # chrome = webdriver.Chrome(executable_path="D:\Automation\Drivers\chromedriver.exe")
+
+    chrome = webdriver.Chrome(options=options)
+    chrome.get("https://yandex.ru")
+    assert "Яндекс" in chrome.title
+    
+    pass
+
+
